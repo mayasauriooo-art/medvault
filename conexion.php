79a0b1d01e035eec5084cd3409f9b4f6b4e8315f
mayasauriooo-art <mayasauriooo@gmail.com>
2026-04-29 12:07:22 -0600

    Delete conexion.php

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Conexión a la base de datos medv
-$conn = mysqli_connect("localhost", "root", "", "medv");
-
-if (!$conn) {
-    die("Error de conexión: " . mysqli_connect_error());
-}
-?>
